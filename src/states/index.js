2c5545086d38cf94f107a2fa9a78b57556140360
firmann19 <firman19@gmail.com>
2022-11-29 14:05:22 +0700

    To-do App

---
+++ src/states/index.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, createStore } from "redux";
+import { thunk, todoDeletionCheck } from "./middlewares";
+import rootReducer from "./rootReducer";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, todoDeletionCheck)
+);
+
+export { store };
